e0ff7fffa82168b87d997c9636e63ba207e24067
SmitaKankayya <smitakankayya@gmail.com>
2024-05-13 22:46:28 +0530

    initial Commit

---
+++ EmailPasswordCredentialValidator/EmailPasswordCredentialValidator.h
@@ -0,0 +1,18 @@
+//
+//  EmailPasswordCredentialValidator.h
+//  EmailPasswordCredentialValidator
+//
+//  Created by Smita Kankayya on 13/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmailPasswordCredentialValidator.
+FOUNDATION_EXPORT double EmailPasswordCredentialValidatorVersionNumber;
+
+//! Project version string for EmailPasswordCredentialValidator.
+FOUNDATION_EXPORT const unsigned char EmailPasswordCredentialValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmailPasswordCredentialValidator/PublicHeader.h>
+
+
